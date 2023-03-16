ac3544b3e0fdd4a6c7e5de541551818f3832dfe7
savin000 <58307835+savin000@users.noreply.github.com>
2023-03-16 18:05:21 +0300

    add autogenerated project template

---
+++ src/main/java/org/rerooms/roomsandusersservice/RoomsAndUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package org.rerooms.roomsandusersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomsAndUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomsAndUsersServiceApplication.class, args);
+	}
+
+}
