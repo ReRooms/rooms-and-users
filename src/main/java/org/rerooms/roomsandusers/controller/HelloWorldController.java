de0b5b754241ba64a6ce3f81f9354b3b179cfaf5
savin000 <58307835+savin000@users.noreply.github.com>
2023-03-16 18:40:18 +0300

    rename package

--- src/main/java/org/rerooms/roomsandusers/controller/HelloWorldController.java
+++ src/main/java/org/rerooms/roomsandusers/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package org.rerooms.roomsandusers.controllers;
+package org.rerooms.roomsandusers.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
